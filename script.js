516a47c3647a98c7da8ccfbbb66e8982f9af7bae
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-04-26 15:48:36 +0200

    Dopo 5 secondi Scompaiono gli elementi dalla pagina

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const numbersCollection = [];
 const numbers = document.getElementById('target-numbers');
 const timer = document.getElementById('time-to-hide');
 let sec = 5;
-
+const userNumber = [];
 
 stampNumbers();
 console.log(numbersCollection);
-const myInterval = setInterval(timerToHide, 1000);
-setInterval(stopFunction, 1000);
-
+const myInterval = setInterval(gestisciInterval, 1000);
 
 function timerToHide(){  
-  timer.innerText = `Hai ancora ${sec--} secondi per memorizzare i numeri sopra `;
-    
+  timer.innerText = `Hai ancora ${sec} secondi per memorizzare i numeri sopra `;   
 }
-
 
 function stampNumbers(){
   for (let i = 0; i < 5; i++) {
@@ -26,17 +22,39 @@ function stampNumbers(){
   } 
 }
 
-function stopFunction() {
-  if(sec == 0){
-  clearInterval(myInterval);
+function gestisciInterval() {
+
+  timerToHide();
+  sec--;
+  console.log('secondi', sec);
+
+  if(sec < 0){
+
+    clearInterval(myInterval);
+    hideElement();
+
   }
+
 }
 
 function hideElement(){
+
   numbers.classList.add('d-none');
+  timer.classList.add('d-none');
+
 }
 
 function getRndInteger( min, max ) {
+
   return Math.floor(Math.random() * (max - min + 1) ) + min;
+
 }
 
+function getUserInput (){
+
+  for (let i = 0; i < 5; i++) {
+    let num
+    
+  }
+  
+}
